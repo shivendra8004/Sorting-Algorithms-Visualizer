561f7cb69af9f467a303f10e77536f3f3a2823f0
SHIVENDRA8004 <jat13632@gmail.com>
2024-03-01 03:02:05 +0530

    Feat: Updated the ui and some small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,8 +34,8 @@ const Home = () => {
         <main className="absolute top-0 h-screen w-screen z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#150229_1px)] bg-[size:40px_40px]">
             <div className="flex h-full justify-center">
                 <div className="flex max-w-[1020px] w-full flex-col lg:px-0 px-4" id="content-container">
-                    <div className="h-[66px] relative flex items-center justify-between w-full">
-                        <h1 className="text-gray-300 text-xl font-light hidden md:flex">Sorting Algorithms Visualizer</h1>
+                    <div className="h-[66px] relative block md:flex items-center justify-between w-full">
+                        <h1 className="text-gray-300 text-xl font-light text-center py-4 my-2 border-b sm:p-0 sm:border-0">Sorting Algorithms Visualizer</h1>
                         <div className="flex items-center justify-center gap-4">
                             <Slider isDisabled={isSorting} value={animationSpeed} handleChange={(e) => setAnimationSpeed(Number(e.target.value))} />
                             <Select options={algorithmOptions} defaultValue={selectedAlgorithm} onChange={handleSelectChange} isDisabled={isSorting} />
@@ -43,13 +43,13 @@ const Home = () => {
                                 {requiresReset ? <RotateCcw className="text-gray-400 " size={18} /> : <PlayCircle className="text-system-green60" size={18} />}
                             </button>
                         </div>
-                        <div className="hidden sm:flex absolute top-[120%] left-0 w-full">
+                        <div className="flex absolute top-[180%] sm:top-[120%] left-0 w-full">
                             <div className="flex w-full text-gray-400 p-4 rounded border border-system-purple20 bg-system-purple80 bg-opacity-10 gap-6 ">
-                                <div className="flex flex-col items-start justify-start w-3/4 ">
+                                <div className="flex flex-col items-start justify-start w-full sm:w-3/4">
                                     <h3 className="text-lg">{sortingAlgorithmsData[selectedAlgorithm].title}</h3>
                                     <p className="text-sm text-grey-500 pt-2">{sortingAlgorithmsData[selectedAlgorithm].description}</p>
                                 </div>
-                                <div className="flex flex-col items-start justify-start w-1/4 gap-2">
+                                <div className="hidden sm:flex flex-col items-start justify-start w-1/4 gap-2">
                                     <h3 className="text-lg">Time Complexity</h3>
                                     <div className="flex flex-col gap-2">
                                         <p className="flex w-full text-sm text-grey-500">
